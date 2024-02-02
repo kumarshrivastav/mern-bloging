74a3386679d67215d3ac050c4566fc9bbd72fe7b
ANKITKUMAR <ankitkumarjl361@gmail.com>
2024-02-03 02:00:49 +0530

    Install ReactJS and Tailwind CSS

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h2 className="bg-red-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia commodi maiores beatae cupiditate maxime facilis, eveniet nihil perspiciatis id error deleniti voluptatibus temporibus ex vero. Doloremque atque excepturi ratione fuga?</h2>
+    </div>
+  )
+}
+
+export default App
