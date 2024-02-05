83271e60db313e515841f3f789c6ff0156eda57e
ANKITKUMAR <ankitkumarjl361@gmail.com>
2024-02-05 17:53:29 +0530

    dark mode functionality completed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Navbar, TextInput, Button, Dropdown, Avatar } from "flowbite-react";
 // import { AiOutlineSearch } from "react-icons/ai";
 import { IoIosSearch } from "react-icons/io";
-import {FaMoon} from "react-icons/fa"
-import {useSelector} from "react-redux"
-import { Link,useLocation } from "react-router-dom";
+import { FaMoon,FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
+import { toggleTheme } from "../redux/theme/themeSlice.js";
+// import { toggleTheme } from "../redux/theme/themeSlice.js";
 const Header = () => {
-  const {currentUser}=useSelector((state)=>state.user)
-  const location=useLocation()
-  const path=location.pathname
+  const { currentUser } = useSelector((state) => state.user);
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const path = location.pathname;
+  const {theme}=useSelector(state=>state.theme)
   return (
     <Navbar className="border-b-2">
       <Link
@@ -29,47 +33,59 @@ const Header = () => {
         />
       </form>
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
-        <IoIosSearch/>
+        <IoIosSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-          <FaMoon/>
+        <Button
+          className="w-12 h-10 hidden sm:inline"
+          color="gray"
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme ==="light" ? <FaSun/>:<FaMoon />}
+          
         </Button>
         {currentUser ? (
-          <Dropdown arrowIcon={false} inline label={
-            <Avatar alt="user" img={currentUser.profilePicture} rounded/>
-          }>
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
+          >
             <Dropdown.Header>
               <span className="block text-sm">@{currentUser.username}</span>
-              <span className="block text-sm font-medium truncate">{currentUser.email}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
             </Dropdown.Header>
             <Link to="/dashboard?tab=profile">
               <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
-            <Dropdown.Divider/>
+            <Dropdown.Divider />
             <Dropdown.Item>Sign Out</Dropdown.Item>
           </Dropdown>
-        ):(
-<Link to="sign-in">
-<Button gradientDuoTone='purpleToBlue' outline>
-  Sign In
-</Button>
-</Link>
+        ) : (
+          <Link to="sign-in">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In
+            </Button>
+          </Link>
         )}
-        
-        <Navbar.Toggle/>
+
+        <Navbar.Toggle />
       </div>
-        <Navbar.Collapse>
-          <Navbar.Link active={path==="/"} as={"div"}>
-            <Link to="/">Home</Link>
-          </Navbar.Link>
-          <Navbar.Link active={path==="/about"} as={"div"}>
-            <Link to="/about">About</Link>
-          </Navbar.Link>
-          <Navbar.Link active={path==="/projects"} as={"div"}>
-            <Link to="/projects">Projects</Link>
-          </Navbar.Link>
-        </Navbar.Collapse>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import {configureStore,combineReducers} from "@reduxjs/toolkit"
 import userReducer from "./user/userSlice.js"
 import {persistReducer, persistStore} from "redux-persist"
+import themeReducer from "./theme/themeSlice.js"
 import storage from "redux-persist/lib/storage"
 // import { buildGetDefaultMiddleware } from "@reduxjs/toolkit/dist/getDefaultMiddleware.js"
 const rootReducer=combineReducers({
     user:userReducer,
+    theme:themeReducer
 })
 const persistConfig={
     key:'root',
